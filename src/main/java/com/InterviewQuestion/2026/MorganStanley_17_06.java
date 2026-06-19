f797300ad3c52c358bbbbac55a1c83476bf6af39
developerarunsingh2 <121679989+developerarunsingh2@users.noreply.github.com>
2026-06-19 10:13:23 +0530

    updated interview

---
+++ src/main/java/com/InterviewQuestion/2026/MorganStanley_17_06.java
@@ -0,0 +1,81 @@
+//package com.practice.May_June_26;
+//
+//public class MorganStanley {
+//    //two sum problem
+//
+//    f(input arr = 3, 4, 9,2)
+//    target: 7
+//
+//    Key:
+//            3,0
+//            4,1
+//            9,2
+//            2,3
+//
+//
+//    output : sum index = 0,1
+//
+//    string test = "I am arun, arun"
+//
+//1,1,2
+//
+//    Map<String, Integer> m = new hashMap<>();
+//
+//    String [] strList = test.split("//");
+//
+//
+//for(String s : strList)
+//    {
+//        if(map.get(s).equals(s))
+//        {
+//            map.put(s,1);
+//        }
+//
+//        else{
+//            map.put(s, map.get(s)+1));
+//
+//        }
+//
+//
+//        public class Singleton{
+//
+//            private static Singleton Instance;
+//
+//            private Singleton(){}
+//            public static syncronize getInstance(){
+//
+//                if(Instance==null)
+//                {
+//                    return new Singleton();
+//                }
+//                return null;
+//
+//            }
+//
+//            Order name, address, pickup, drop
+//
+//            OrderBuider
+//
+//return Order
+//
+//            Main:
+//
+//                    OrderBuilder.name()
+//                    .build();
+//
+//            Huge data: lp-> delay:
+//                    1. frontend
+//            opt:
+//
+//                    2. backend
+//
+//
+//                    p ; 100 ,
+//            c 3
+//
+//                    1000
+//
+// scaling in kafka
+// In singleton class in multithreaded environment
+// AI use case : what all things you have used in AI and in what scenario.
+// }
---
+++ src/main/java/com/InterviewQuestion/2026/Turvo_round2_15_06_26.java
@@ -0,0 +1,24 @@
+//public class Turvo_round2_15_06_26 {
+//    +ve,
+//
+//
+//    public class Main {
+//
+//        public static void main(String args[])
+//        {
+//
+//            System.out.println("Hello");
+//
+//        }
+//    }
+//
+//    conference room: 10,
+//            50
+//
+//    seat booking: time slot: 1 hour.
+//10 min reminder
+//
+//
+//
+//
+//}
